1f49dccfcabffccfd10e294551167c0d0c77f044
eli2.0 <80164731+negreteliza@users.noreply.github.com>
2024-11-30 16:46:13 +0200

    Script Commit
    am uitat sa-l adaug...

---
+++ Scripts/GameScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    [SerializeField] RectTransform zeiger;
+    [SerializeField] Image area;
+    [SerializeField] float areaAngle = 30;
+    [SerializeField] float moveSpeed = 1;
+    [SerializeField] float areaStartAngle = 270;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        var eu = zeiger.transform.eulerAngles;
+        eu.z += Time.deltaTime * moveSpeed;
+        float x = eu.z;
+        zeiger.transform.eulerAngles = eu;
+
+
+        eu = area.transform.eulerAngles;
+        eu.z = areaStartAngle - 270;
+        area.transform.eulerAngles = eu;
+
+        float a = areaStartAngle;
+        float b = areaStartAngle - areaAngle;
+
+        area.fillAmount = areaAngle / 360.0f;
+
+        print((a < x && x < b)  +" " + a + " " + x + " " + b); 
+    }
+
+}
